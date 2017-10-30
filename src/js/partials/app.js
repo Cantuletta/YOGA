0b8050c8a97d78d0e262b8cbe7c8593b0112967d
Cantu <cantuletta@gmail.com>
2017-10-30 17:43:56 +0200

    фул сайт ++

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,17 @@
 jQuery(function ($) {
     $("#trialForm").validate();
+
+    $('.buy-link').hover(
+        function () {
+            $(this)
+                .closest('.block-feature')
+                .find('.basic-pro-premium')
+                .addClass('hovered');
+        },
+        function () {
+            $(this)
+                .closest('.block-feature')
+                .find('.basic-pro-premium')
+                .removeClass('hovered');
+        });
 });
